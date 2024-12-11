81c55a174eccaa83e9edae1fa9eb9daff3bf554c
Aadi Swadipto Mondal <silverswordaadi05@gmail.com>
2024-12-10 21:41:44 -0500

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+    // Flush after every std::cout / std:cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
-  
-  std::string input;
-  std::getline(std::cin, input);
-  std::cout << input << ": command not found" << std::endl;
+    // Uncomment this block to pass the first stage
+
+    while (true) {
+        std::cout << "$ ";
+        std::string input;
+        std::getline(std::cin, input);
+        if (input == "exit") {
+            break;
+        }
+        std::cout << input << ": command not found" << std::endl;
+    }
 }
